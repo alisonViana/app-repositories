47cfa356280c591caa5aa3cb0cf040e94cfc2467
alisonViana <alisonviana.c@gmail.com>
2021-10-19 10:55:42 -0300

    created viewModel

---
+++ app/src/main/java/br/com/dio/app/repositories/presentation/di/PresentationModel.kt
@@ -0,0 +1,20 @@
+package br.com.dio.app.repositories.presentation.di
+
+import br.com.dio.app.repositories.presentation.MainViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.core.module.Module
+import org.koin.dsl.module
+
+object PresentationModel {
+
+    fun load() {
+        viewModelModule()
+    }
+
+    private fun viewModelModule(): Module {
+        return module {
+            viewModel { MainViewModel(get())}
+        }
+    }
+
+}
